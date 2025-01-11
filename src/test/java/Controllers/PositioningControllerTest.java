7d3b62e5ef33c7a9fca4512c110199416d4d7e94
PcMac820 <panimcdermott@gmail.com>
2025-01-11 17:06:04 +0000

    Delete README.md

--- src/test/java/Controllers/PositioningControllerTest.java
+++ src/test/java/Controllers/PositioningControllerTest.java
@@ -2,22 +2,17 @@ package Controllers;
 
 import com.example.ilp_cw1.Controllers.*;
 import com.example.ilp_cw1.DTO.*;
-import com.example.ilp_cw1.Services.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.InjectMocks;
-import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 
-import java.time.LocalDate;
-import java.time.YearMonth;
 import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
-import static org.mockito.Mockito.when;
 
 public class PositioningControllerTest {
 
